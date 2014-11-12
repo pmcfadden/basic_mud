90b92f3efd4c0b7917f47fbbabe8dfbf6c6b467a
Patrick McFadden <pemcfadden@gmail.com>
2014-11-11 21:02:29 -0600

    PM Adding initial server

--- lib/classless_mud.rb
+++ lib/classless_mud.rb
@@ -1,4 +1,17 @@
+require 'socket'
+
 require "classless_mud/version"
 
 module ClasslessMud
+  server = TCPServer.new 2000
+
+  puts "Starting server on port 2000"
+  loop do
+    Thread.start(server.accept) do |client|
+      client.puts "Enter name:"
+      name = client.gets
+      client.puts "Hello #{name}"
+      client.close
+    end
+  end
 end
