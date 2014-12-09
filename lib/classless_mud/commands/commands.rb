73d449909fa5f0494109815244f193c9f15dfd0a
Derek Hammer <derek.r.hammer@gmail.com>
2014-12-09 09:40:19 -0800

    Add the commands command

---
+++ lib/classless_mud/commands/commands.rb
@@ -0,0 +1,26 @@
+module ClasslessMud
+  module Commands
+    # This is the commands command. The naming is weird, but this lists out all available commands
+    class Commands
+      COMMANDS_TO_HIDE = [BadCommand]
+
+      def self.perform game, player, message
+        commands = ClasslessMud::Commands.constants
+                     .select { |c| Class === ClasslessMud::Commands.const_get(c) }
+                     .reject { |c| COMMANDS_TO_HIDE.include?(ClasslessMud::Commands.const_get(c)) }
+                     .map { |c| c.to_s.downcase }
+        player.puts columnize(commands)
+      end
+
+      def self.columnize array, width=4
+        result = ''
+        longest_char = array.max_by(&:size).size
+        array.each_slice(width) do |row|
+          spaced_rows = row.map { |word| "%#{longest_char}s" % word }
+          result += spaced_rows.join(" ") + "\n"
+        end
+        result
+      end
+    end
+  end
+end
