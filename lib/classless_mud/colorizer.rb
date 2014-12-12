f07590c1f0dc4c340ee5dccdffba967468735df7
Derek Hammer <derek.r.hammer@gmail.com>
2014-12-12 00:27:12 -0800

    Moar colors

--- lib/classless_mud/colorizer.rb
+++ lib/classless_mud/colorizer.rb
@@ -1,9 +1,24 @@
 module ClasslessMud
   module Colorizer
+    # To add more, look at this
+    # alias colortest="python -c \"print('\n'.join([(' '.join([('\033[38;5;' + str((i + j)) + 'm' + str((i + j)).ljust(5) + '\033[0m') if i + j < 256 else '' for j in range(10)])) for i in range(0, 256, 10)]))\""
     CODES = {
-      red: 31,
-      green: 32,
-      yellow: 33
+      red:           '38;5;1',
+      green:         '38;5;2',
+      yellow:        '38;5;3',
+      blue:          '38;5;4',
+      purple:        '38;5;5',
+      teal:          '38;5;7',
+      white:         '38;5;8',
+      black:         '38;5;16',
+      bright_blue:   '38;5;27',
+      grey:          '38;5;59',
+      bright_teal:   '38;5;81',
+      bright_green:  '38;5;83',
+      bright_red:    '38;5;160',
+      bright_white:  '38;5;195',
+      bright_yellow: '38;5;226',
+      bright_purple: '38;5;207',
     }
 
     def colorize(input, color)
@@ -11,16 +26,8 @@ module ClasslessMud
       "\e[#{color_code}m#{input}\e[0m"
     end
 
-    def red(input)
-      colorize(input, :red)
-    end
-
-    def green(input)
-      colorize(input, :green)
-    end
-
-    def yellow(input)
-      colorize(input, :yellow)
+    CODES.keys.each do |color|
+      define_method(color) { |input| colorize(input, color) }
     end
   end
 end
--- lib/classless_mud/commands/score.rb
+++ lib/classless_mud/commands/score.rb
@@ -1,8 +1,10 @@
 module ClasslessMud
   module Commands
     class Score
+      extend ::ClasslessMud::Colorizer
+
       def self.perform game, player, message
-        player.puts "You have #{player.health} (#{player.max_health}) hit points"
+        player.puts "You have #{bright_green(player.health)} (#{green(player.max_health)}) hit points"
       end
     end
   end
