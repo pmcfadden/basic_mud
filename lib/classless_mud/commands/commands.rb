9c133798e61bcfcf44cfd76bc8b28c82a9426d18
Derek Hammer <hammer@squareup.com>
2016-11-27 22:43:19 -0800

    Show admin commands

--- lib/classless_mud/commands/commands.rb
+++ lib/classless_mud/commands/commands.rb
@@ -13,8 +13,20 @@ module ClasslessMud
            .map { |c| c.to_s.downcase }
       end
 
+      def self.admin_commands
+        ClasslessMud::Commands::Admin.constants
+           .select { |c| Class === ClasslessMud::Commands::Admin.const_get(c) }
+           .reject { |c| COMMANDS_TO_HIDE.include?(ClasslessMud::Commands::Admin.const_get(c)) }
+           .map { |c| c.to_s.downcase }
+      end
+
       def self.perform game, player, message
         player.puts columnize(commands)
+        if player.admin?
+          player.puts "Admin Commands"
+          player.puts "=============="
+          player.puts columnize(admin_commands)
+        end
       end
 
       def self.columnize array, width=4
